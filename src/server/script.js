5f19bd35550ed5025b69aa0a3d16422897a5067e
System Administrator <admin@example.com>
2025-01-27 03:45:52 +0000

    js changes commit

--- src/server/script.js
+++ src/server/script.js
@@ -1,8 +1,9 @@
 import { gs } from '@servicenow/glide'
+import {kushal} from './gupta'
 
 export function showStateUpdate(current, previous) { 
     const currentState = current.getValue('state')
     const previousState = previous.getValue('state')
-
+    kushal();
     gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
 }
